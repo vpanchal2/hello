3a580930674fac5b2b129f9f90cbabb6fabedf45
vpanchal2 <vpanchal2@unl.edu>
2020-08-22 17:07:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Author: Vishal pancchal
+ * Date: 2020/08/22
+ * 
+ * A simple program
+ * 
+ */
+  #include<stdlib.h>
+  #include<stdio.h>
+  
+  int main(int argc, char **argv) {
+    
+    printf("vishal panchal!\n");
+    printf("computer science\n");
+
+  return 0;
+  
+}
